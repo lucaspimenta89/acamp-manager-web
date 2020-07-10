4ac6e035ee81130c264e3b35b9e5d14671cc1cb8
Lucas Pimenta <lucas.duane@gmail.com>
2020-07-10 08:48:27 -0400

    setting api url

--- src/config/Env.ts
+++ src/config/Env.ts
@@ -1,8 +1,10 @@
 export default class Env {
   static getEnv() {
-    const apiBaseUrl = process.env.NODE_ENV === 'production'
-      ? 'https://acamp-manager-api.herokuapp.com/api'
-      : 'http://localhost:4000/api'
+    // const apiBaseUrl = process.env.NODE_ENV === 'production'
+    //   ? 'https://acamp-manager-api.herokuapp.com/api'
+    //   : 'http://localhost:4000/api'
+
+    const apiBaseUrl = 'https://acamp-manager-api.herokuapp.com/api'
 
     return {
       apiBaseUrl,
